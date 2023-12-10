7979677fcd15638bed1acdfabd568f4141403f06
lukasRevol <lukasz.revol@gmail.com>
2023-12-10 11:29:16 +0100

    maybe that movement function will do?
    previous movement function had lag when changing directions. Now it looks better but still doesn's work propely when near walls.
    Next time maybe i will try something with async function and await so first the code will execute and then it will wait

--- js/main.js
+++ js/main.js
@@ -2,110 +2,73 @@ const snakeHead = document.querySelector('.snake-head');
 const food = document.querySelector('.food');
 
 let x = 10;
-let y = 20;
+let y = 10;
 
-let foodX, foodY
+let foodX, foodY;
 
-let speed = 300
+let speed = 300;
 
-let upInterval, downInterval, leftInterval, rightInterval
+let moveUp, moveDown, moveLeft, moveRight;
 
 xAxisAllowed = true;
 yAxisAllowed = true;
 
 const generateFood = () => {
-    foodX = parseInt(Math.random()*20+1)
-    foodY = parseInt(Math.random()*20+1)
-    food.style.cssText = `grid-row: ${foodY}; grid-column: ${foodX};`;
-}
-
+	foodX = parseInt(Math.random() * 20 + 1);
+	foodY = parseInt(Math.random() * 20 + 1);
+	food.style.cssText = `grid-row: ${foodY}; grid-column: ${foodX};`;
+	speed = speed - 5;
+};
 generateFood();
 
+let xWall, xOtherWall, yWall, yOtherWall, xDirection, yDirection;
+yDirection = 0;
+xDirection = 0;
 
-
-const moveUp = () => {
-    clearInterval(leftInterval);
-    clearInterval(rightInterval);
-    if (yAxisAllowed) {
-        yAxisAllowed = false
-        xAxisAllowed = true
-        upInterval = setInterval(() => {
-            (y <= 1)? y = 20 : y--;
-            console.log(y);
-            snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
-        }, speed);
-    } 
-}
-
-const moveDown = () => {
-    clearInterval(leftInterval);
-    clearInterval(rightInterval);
-    if (yAxisAllowed) {
-        yAxisAllowed = false
-        xAxisAllowed = true
-        downInterval = setInterval(() => {
-            (y >=20)? y = 1 : y++;
-            console.log(y);
-            snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
-        }, speed);
-    } 
-}
-
-const moveRight = () => {
-    clearInterval(upInterval);
-    clearInterval(downInterval);
-    if (xAxisAllowed) {
-        xAxisAllowed = false
-        yAxisAllowed = true
-        rightInterval = setInterval(() => {
-            (x >=20)? x = 1 : x++;
-            console.log(x);
-            snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
-        }, speed);
-    } 
-}
-
-const moveLeft = () => {
-    clearInterval(upInterval);
-    clearInterval(downInterval);
-    if (xAxisAllowed) {
-        xAxisAllowed = false
-        yAxisAllowed = true
-        leftInterval = setInterval(() => {
-            (x <= 1)? x = 20 : x--;
-            console.log(x);
-            snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
-        }, speed);
-    } 
-}
 
 setInterval(() => {
-    if (x === foodX && y === foodY){
-        generateFood();
-    }
+	x == xWall ? (x = xOtherWall) : (x = x + 1 * xDirection);
+	y == yWall ? (y = yOtherWall) : (y = y + 1 * yDirection);
+	console.log(`x:${x} y:${y}`);
+    if (x === foodX && y === foodY) {
+		generateFood();
+	}
+	snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
 }, speed);
 
-
-
 const movement = (e) => {
-	if (e.key == 'ArrowUp') {
-        console.log(e.key);
-		moveUp();
+	if (e.key == 'ArrowUp' && yDirection == 0) {
+		console.log(e.key);
+		yWall = 1;
+		yOtherWall = 20;
+		xDirection = 0;
+		yDirection = -1;
+		moveSnake();
 	}
-	if (e.key == 'ArrowDown') {
-        console.log(e.key);
-		moveDown();
+	if (e.key == 'ArrowDown' && yDirection == 0) {
+		console.log(e.key);
+		yWall = 20;
+		yOtherWall = 1;
+		xDirection = 0;
+		yDirection = 1;
+		moveSnake();
 	}
-	if (e.key == 'ArrowLeft') {
-        console.log(e.key);
-		moveLeft();
+	if (e.key == 'ArrowLeft' && xDirection == 0) {
+		console.log(e.key);
+		xWall = 1;
+		xOtherWall = 20;
+		xDirection = -1;
+		yDirection = 0;
+		moveSnake();
 	}
-	if (e.key == 'ArrowRight') {
-        console.log(e.key);
-		moveRight();
+	if (e.key == 'ArrowRight' && xDirection == 0) {
+		console.log(e.key);
+		xWall = 20;
+		xOtherWall = 1;
+		xDirection = 1;
+		yDirection = 0;
+		moveSnake();
 	}
-    
-    
 };
 
 window.addEventListener('keydown', movement);
