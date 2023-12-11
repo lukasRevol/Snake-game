6503688c65d880cb5d3d6c3537f6c276c2735468
lukasRevol <lukasz.revol@gmail.com>
2023-12-11 23:02:26 +0100

    snake tail
    every snake needs a tail, maybe even this green box will get one one day

--- js/main.js
+++ js/main.js
@@ -1,38 +1,59 @@
 const snakeHead = document.querySelector('.snake-head');
+const board = document.querySelector('.box');
 const food = document.querySelector('.food');
-let x = 2;
-let y = 2;
+
+// live collection
+const tailBox = document.getElementsByClassName('tail');
+
 let foodX, foodY;
-let speed = 200;
+let speed = 300;
 let xDirection, yDirection;
 const startWall = 1;
 const endWall = 20;
 yDirection = 0;
 xDirection = 0;
 
+let x = [2];
+let y = [2];
 
 setInterval(() => {
-	x = x + 1 * xDirection;
-	y = y + 1 * yDirection;
-	
-	if (x > endWall) {
-		x = startWall;
-	}
-	if (x < startWall) {
-		x = endWall;
-	}
-	if (y > endWall) {
-		y = startWall;
-	}
-	if (y < startWall) {
-		y = endWall;
-	}
-	snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
-
-	if (x === foodX && y === foodY) {
+	if (x[0] === foodX && y[0] === foodY) {
 		generateFood();
+		generatTail();
 	}
+	console.log(tailBox);
+	for (let i = 0; i < tailBox.length; i++) {
+		x[i] = x[i] + 1 * xDirection;
+
+		y[i] = y[i] + 1 * yDirection;
+
+		if (x[i] > endWall) {
+			x[i] = startWall;
+		}
+		if (x[i] < startWall) {
+			x[i] = endWall;
+		}
+		if (y[i] > endWall) {
+			y[i] = startWall;
+		}
+		if (y[i] < startWall) {
+			y[i] = endWall;
+		}
+	if (tailBox.length > 0){
+		tailBox[i].style.cssText = `grid-row: ${y[i]}; grid-column: ${x[i]};`;}
+	}
+		snakeHead.style.cssText = `grid-row: ${y[0]}; grid-column: ${x[0]};`;
+	
+	
 }, speed);
+
+const generatTail = () => {
+	const tail = document.createElement('div');
+	tail.classList.add('tail');
+	board.appendChild(tail);
+	x.unshift(x[0]);
+	y.unshift(y[0]);
+};
 
 const generateFood = () => {
 	foodX = parseInt(Math.random() * 20 + 1);
