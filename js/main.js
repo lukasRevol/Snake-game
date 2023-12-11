2e2b18ab449b41d1ef8dcc563dbc686127892cac
lukasRevol <lukasz.revol@gmail.com>
2023-12-11 21:19:04 +0100

    fixed a little bit movement

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
 const snakeHead = document.querySelector('.snake-head');
 const food = document.querySelector('.food');
-
-let x = 10;
-let y = 10;
-
+let x = 2;
+let y = 2;
 let foodX, foodY;
+let speed = 200;
+let xDirection, yDirection;
+const startWall = 1;
+const endWall = 20;
+yDirection = 0;
+xDirection = 0;
 
-let speed = 300;
 
-let moveUp, moveDown, moveLeft, moveRight;
+setInterval(() => {
+	x = x + 1 * xDirection;
+	y = y + 1 * yDirection;
+	
+	if (x > endWall) {
+		x = startWall;
+	}
+	if (x < startWall) {
+		x = endWall;
+	}
+	if (y > endWall) {
+		y = startWall;
+	}
+	if (y < startWall) {
+		y = endWall;
+	}
+	snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
 
-xAxisAllowed = true;
-yAxisAllowed = true;
+	if (x === foodX && y === foodY) {
+		generateFood();
+	}
+}, speed);
 
 const generateFood = () => {
 	foodX = parseInt(Math.random() * 20 + 1);
@@ -21,53 +42,22 @@ const generateFood = () => {
 };
 generateFood();
 
-let xWall, xOtherWall, yWall, yOtherWall, xDirection, yDirection;
-yDirection = 0;
-xDirection = 0;
-
-
-setInterval(() => {
-	x == xWall ? (x = xOtherWall) : (x = x + 1 * xDirection);
-	y == yWall ? (y = yOtherWall) : (y = y + 1 * yDirection);
-	console.log(`x:${x} y:${y}`);
-    if (x === foodX && y === foodY) {
-		generateFood();
-	}
-	snakeHead.style.cssText = `grid-row: ${y}; grid-column: ${x};`;
-}, speed);
-
 const movement = (e) => {
 	if (e.key == 'ArrowUp' && yDirection == 0) {
-		console.log(e.key);
-		yWall = 1;
-		yOtherWall = 20;
 		xDirection = 0;
 		yDirection = -1;
-		moveSnake();
 	}
 	if (e.key == 'ArrowDown' && yDirection == 0) {
-		console.log(e.key);
-		yWall = 20;
-		yOtherWall = 1;
 		xDirection = 0;
 		yDirection = 1;
-		moveSnake();
 	}
 	if (e.key == 'ArrowLeft' && xDirection == 0) {
-		console.log(e.key);
-		xWall = 1;
-		xOtherWall = 20;
 		xDirection = -1;
 		yDirection = 0;
-		moveSnake();
 	}
 	if (e.key == 'ArrowRight' && xDirection == 0) {
-		console.log(e.key);
-		xWall = 20;
-		xOtherWall = 1;
 		xDirection = 1;
 		yDirection = 0;
-		moveSnake();
 	}
 };
 
